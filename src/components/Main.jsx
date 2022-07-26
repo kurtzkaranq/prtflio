b86bf5ff43189ea6f8c9fe2e15b4fb129330aa19
kurtzkaranq <mendbayarerxes@gmail.com>
2022-07-26 20:52:50 +0800

    from home;

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import { HiArrowNarrowRight} from 'react-icons/hi'
 
 export default function Main() {
   return (
-      <div name ='main'className='bg-[#0a192f] w-full h-screen'>
+      <div name ='main'className=' section1 bg-[#0a192f] w-full h-screen'>
           <div className='max-w-[1000px] ml-[50px] mx-auto px-8 flex flex-col justify-center h-full text-left'>
               <p className='text-pink-600 text-2xl'>hi , my name is</p>
               <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Erkhes Mendbayar</h1>
